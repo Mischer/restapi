10bf06b3d0032b6d1269de029c19dd21982ec9c3
Mikhail Kultyiasau <mikhail.kultiyasov@tispr.com>
2024-09-30 16:10:02 +0200

    Initial commit

---
+++ src/test/java/com/mk/restapi/TestRestapiApplication.java
@@ -0,0 +1,11 @@
+package com.mk.restapi;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(RestapiApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
