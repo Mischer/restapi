f8b00c2bdd178a5b490289f5f8a499af74c96492
Mikhail Kultyiasau <mikhail.kultiyasov@tispr.com>
2024-10-11 13:48:21 +0200

    Integrate chessboardjs&chessjs on client side

--- angular-auth/src/app/app.routes.ts
+++ angular-auth/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './components/signup.component';
 import { SigninComponent } from './components/signin.component';
 import {WebSocketTestComponent} from "./web-socket-test/web-socket-test.component";
+import {ChessboardComponent} from "./chessboard/chessboard.component";
 
 export const appRoutes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'ws', component: WebSocketTestComponent },
+  { path: 'play', component: ChessboardComponent },
   { path: '', redirectTo: '/signin', pathMatch: 'full' }
 ];
